7c9043f1e52d0d58f2f94c31e27b3625437c818f
luupanu <22541529+luupanu@users.noreply.github.com>
2021-11-10 22:41:48 +0200

    wipwip

--- Assets/Scripts/DialogueParser.cs
+++ Assets/Scripts/DialogueParser.cs
@@ -17,32 +17,80 @@ public class DialogueParser
         // make sure there is an escape from this conversation
         bool hasEndKeyword = false;
 
+        TreeNode<Dialogue> dialogueTree;
+        TreeNode<Dialogue> currentNode;
+
+        int i = 1;
+        int indentation = 0;
+
         foreach (string line in File.ReadLines(filename))
         {
             if (string.IsNullOrWhiteSpace(line)) continue; // empty line
 
+            // Count indentation. Accept only spaces, no tabs.
+            int prevIndentation = indentation;
+            int indentation = line.TakeWhile(c => c.Equals(' ')).Count();
+
+            if ((indentation - prevIndentation) % 4 != 0)
+            {
+                Debug.LogError("DialogueFileError: Indentation in dialogue files should be 4.");
+                break;
+            }
+
             string trimmed = line.Trim();
 
-            if (trimmed.Substring(0, 1) == "#") continue; // comment line
+            if (trimmed.Substring(0, 1) == "#") continue; // comment
 
-            if (trimmed.Substring(0, 1) == "{") // command line
+            if (trimmed.Substring(0, 1) == "{") // command
             {
                 if (trimmed.Equals("{END}")) hasEndKeyword = true;
             }
-            else // dialogue line
+            else // dialogue
             {
                 // 1st capture group: the speaker
                 // 2nd capture group: the line
                 // 3rd capture group: command or condition
-                Regex regex = new Regex(@"^(\w+[ :])+ (\w+[ ,.!'?] ?)+(\{.*\})?$", RegexOptions.Singleline);
-
+                // 4th capture group: bonuses or penalties
+                Regex regex = new Regex(@"^([\w ]+)+: ([\w ,.!'?]+) ?(\{?.*\})? ?(\[.*\])?$", RegexOptions.Singleline);
                 Match match = regex.Match(trimmed);
+
+                if (!match.Success)
+                {
+                    Console.LogError("DialogueError: Line " + i + " didn't match the dialogue syntax.");
+                }
+
+                Dialogue dialogue = new Dialogue(match.Groups[1], match.Groups[2], match.Groups[3], match.Groups[4]);
+
+                if (!dialogueTree)
+                {
+                    dialogueTree = new TreeNode<>(dialogueLine);
+                    currentNode = dialogueTree;
+                }
+                else if (indentation > prevIndentation)
+                {
+                    currentNode = dialogueTree.AddChild();
+                }
+                else if (indentation == prevIndentation)
+                {
+                    dialogueTree.AddChild();
+                }
+                else
+                {
+                    while (indentation <= prevIndentation)
+                    {
+                        prevIndentation -= 4;
+                        currentNode = currentNode.Parent;
+                    }
+                    currentNode = dialogueTree.AddChild();
+                }
             }
+
+            i++;
         }
 
         if (!hasEndKeyword)
         {
-            // should error here, dialogue file is bad
+            Debug.LogError("DialogueError: {END} command missing in dialogue file.")
         }
     }
 }
