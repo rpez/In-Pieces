e4183c12c2b1974af4a41ae904f307a3ec15160d
Reetu Kontio <reetu.kontio@gmail.com>
2021-11-17 21:25:51 +0200

    add ui toolkit and start dialog ui

---
+++ Assets/UI/InGame/Dialogs/Dialog.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+[UnityEngine.Scripting.Preserve]
+public class Dialog : MonoBehaviour
+{
+    public VisualElement m_root;
+    public VisualTreeAsset dialogOptionUXML;
+
+    private ListView m_dialogOptions;
+
+    private string[] MOCKDIALOG = new string[3] { "I am dialog option 1.", "Go to next option.", "Choose me" };
+
+    private void Start()
+    {
+        UIDocument doc = GetComponent<UIDocument>();
+        if (doc == null)
+        {
+            Debug.LogError($"{name}: Could not find UIDocument component - Cannot initialize dialog");
+            return;
+        }
+        m_root = doc.rootVisualElement;
+        m_dialogOptions = m_root.Q<ListView>("dialog-options");
+
+        m_dialogOptions.makeItem = dialogOptionUXML.CloneTree;
+        m_dialogOptions.bindItem = (elem, index) => {
+            Character chara = m_characters[index];
+            AssetCard card = new AssetCard(
+                elem,
+                onHovered: null,
+                onClicked: () => {
+                    ChangeTab(m_completableList);
+                    UpdateCompletableList(chara.name);
+                },
+                onDoubleClicked: null);
+            card.Bind(chara);
+        };
+        m_dialogOptions.itemHeight = 200;
+        m_dialogOptions.itemsSource = MOCKDIALOG;
+
+        m_dialogOptions.Refresh();
+    }
+}
