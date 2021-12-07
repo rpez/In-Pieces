b715985675806de32ddd7491343579a59eb3afea
Lmontan-don <60777014+Lmontan-don@users.noreply.github.com>
2021-12-07 10:08:53 +0200

    fmod
    ambiences

---
+++ Assets/Scripts/Sound/Hallway.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+
+
+public class Hallway : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //changing global parameter in fmod. you have to check global paramter in fmod edit paramter window
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("inHallway", 1);
+
+            UnityEngine.Debug.Log("inHallway");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("inHallway", 0);
+            UnityEngine.Debug.Log("NotInHallway");
+        }
+    }
+}
---
+++ Assets/Scripts/Sound/LivingRoom.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+
+
+public class LivingRoom : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //changing global parameter in fmod. you have to check global paramter in fmod edit paramter window
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("inLivingRoom", 1);
+
+            UnityEngine.Debug.Log("inBedroom");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("inLivingRoom", 0);
+            UnityEngine.Debug.Log("NotInBedroom");
+        }
+    }
+}
