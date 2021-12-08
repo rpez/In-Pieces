2951eb98f625d1d5a9a17792b1a2108550c4fbfe
luupanu <22541529+luupanu@users.noreply.github.com>
2021-12-08 15:50:08 +0200

    fix roll_success display?

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -83,9 +83,9 @@ public class UIManager : MonoBehaviour
             string line = condDialogue.Line;
 
             if (condDialogue.Line.StartsWith("*SUCCESS*"))
-                line = line.Replace("[SUCCESS]", "<color=green>[SUCCESS]</color><color=white>");
+                line = line.Replace("*SUCCESS*", "<color=green>[SUCCESS]</color><color=white>");
             else if (condDialogue.Line.StartsWith("*FAILURE*"))
-                line = line.Replace("[FAILURE]", "<color=red>[FAILURE]</color><color=white>");
+                line = line.Replace("*FAILURE*", "<color=red>[FAILURE]</color><color=white>");
 
             dialogText.text = m_rollResult + condDialogue.Line;
             m_rollResult = "";
